987dcc2cc08dbe28d4f2456534cf673fbe8e880e
Tahrim19 <tarimbilal4@gmail.com>
2024-10-24 15:47:43 -0700

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import ProductChart from './components/ProductChart'
+import './app.css'
+import CategoryChart from './components/CategoryChart'
 
 export default function App() {
   return (
-    <div>
-      hi
-    </div>
+    <>
+      <div className='app'>
+        <ProductChart/>
+        <CategoryChart/>
+      </div>
+    </>
   )
 }
+
